f206a76c215b3a9a28677ffe8f575b97ea2bc91c
Aleksey Cherniaev <cherniaevaleksey2304@gmail.com>
2025-08-11 01:06:17 +0300

    feat: add db connection class

---
+++ app/db/database_connection.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+
+import psycopg2
+import structlog
+from psycopg2.extras import DictCursor
+
+logger = structlog.get_logger(__name__)
+
+
+class DatabaseConnection(ABC):
+    def __init__(
+        self, dbname: str, dbuser: str, dbpassword: str, dbhost: str, dbport: int = 5432
+    ) -> None:
+        self._dbname = dbname
+        self._user = dbuser
+        self._password = dbpassword
+        self._host = dbhost
+        self._port = dbport
+        self._connection: psycopg2.extensions.connection | None = None
+
+    @abstractmethod
+    def connect(self) -> None:
+        pass
+
+    @abstractmethod
+    def disconnect(self) -> None:
+        pass
+
+
+class PostgresDatabaseConnection(DatabaseConnection):
+    def connect(self) -> None:
+        try:
+            self._connection = psycopg2.connect(
+                dbname=self._dbname,
+                user=self._user,
+                password=self._password,
+                host=self._host,
+                port=self._port,
+                cursor_factory=DictCursor,
+            )
+            logger.info("Database connection established successfully")
+        except psycopg2.Error as e:
+            logger.error(f"Error connecting to database: {e}")
+            raise
+
+    def disconnect(self) -> None:
+        if self._connection:
+            self._connection.close()
+            logger.info("Database connection closed successfully")
